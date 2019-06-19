3fb01a8b0dc710f78885763066f2810303e01190
GIRI Lalit <lalit.giri@ext.soprasteria.com>
2019-06-19 12:53:50 +0530

    Node Http Example

---
+++ Simple-Http-Server/index.js
@@ -0,0 +1,71 @@
+var http = require("http");
+var fs = require("fs");
+var path = require("path");
+
+const hostname = 'localhost';
+const port = 1234;
+
+const server = http.createServer((req, res) => {
+    console.log(`Request for ${req.url} by method ${req.method}`);
+
+    res.setHeader('Content-Type', 'text/html');
+
+    if (req.method == 'GET') {
+
+        var fileUrl;
+
+        if (req.url == '/')
+            fileUrl = '/index.html'
+        else fileUrl = req.url;
+
+        var filePath = path.resolve('./public' + fileUrl);
+        const fileExt = '.html'
+        if (fileExt == '.html') {
+            fs.exists(filePath, (exists) => {
+                if (!exists) {
+                    res.statusCode = 404;
+                    res.end(`
+                        <html>
+                            <body>
+                                <h1> Error 404: ${fileUrl} not found.
+                            </body>
+                        </html>
+                    `);
+                    return;
+                }
+                res.statusCode = 200;
+                fs.createReadStream(filePath).pipe(res);
+            });
+        }
+        else {
+            res.statusCode = 404;
+            res.end(`
+                        <html>
+                            <body>
+                                <h1> Error 404: ${fileUrl} not a HTML file found.
+                            </body>
+                        </html>
+                    `);
+        }
+
+    }
+    else {
+        res.statusCode = 404;
+        res.end(`
+                    <html>
+                        <body>
+                            <h1> Error 404: ${req.method} not a Supported.
+                        </body>
+                    </html>
+                `);
+    }
+
+
+
+
+
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`)
+});
