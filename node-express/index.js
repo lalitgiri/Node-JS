4cb0ca633bb842eaae963bf2f500dc48f626c92b
GIRI Lalit <lalit.giri@ext.soprasteria.com>
2019-07-15 12:28:34 +0530

    Basic creation of server using express

---
+++ node-express/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const http = require('http');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log(req.headers);
+    res.status = 200;
+    res.setHeader('Content-Type', 'Text/Html');
+    res.end(`
+        <html>
+            <body>
+                <h1>This is an express server.</h1>
+            </body>
+        </html>
+    `);
+});
+
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+    console.log(`server running at http://${hostname}:${port}`);
+});
