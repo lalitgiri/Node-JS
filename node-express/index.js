bc27884506ae44031edf27b137d62a682d3e9f11
GIRI Lalit <lalit.giri@soprasteria.com>
2019-08-01 11:06:58 +0530

    implementation of basic authentication

--- node-express/index.js
+++ node-express/index.js
@@ -4,13 +4,46 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const dishRouter = require('./routes/dishRouter');
-const dishRouterWithId = require('./routes/dishRouterWithId');
+
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
+
+/*  
+
+// basic authentication 
+
+function auth (req, res, next) {
+    console.log(req.headers);
+    var authHeader = req.headers.authorization;
+    if (!authHeader) {
+        var err = new Error('You are not authenticated!');
+        res.setHeader('WWW-Authenticate', 'Basic'); //this is gives a pop to enter password and username.
+        err.status = 401;
+        next(err);
+        return;
+    }
+  
+    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+    var user = auth[0];
+    var pass = auth[1];
+    if (user == 'admin' && pass == 'password') {
+        next(); // authorized
+    } else {
+        var err = new Error('You are not authenticated!');
+        res.setHeader('WWW-Authenticate', 'Basic');      
+        err.status = 401;
+        next(err);
+    }
+  }
+  
+  app.use(auth);
+
+  */
+
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use('/dishes/',dishRouter);
