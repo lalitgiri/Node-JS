56f9db11f8f8fb2b40f984280bb1fadda402619c
lalitgiri <35764185+lalitgiri@users.noreply.github.com>
2020-07-26 17:43:52 +0530

    Parsing request bodies without Express server
    1. how to parse request bodies with simple http server
    2. how to redirect the routes.
    3. status code for redirection = 302
    4. header for redirection {'Location', '${url}'}

---
+++ Parsing_Request_Bodies_Without_Express/index.js
@@ -0,0 +1,40 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    const method = req.method;
+
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write(`<body>
+                    <h1>Enter Message</h1><br/>
+                    <form action="/message" method="POST">
+                        <input type="text" name="message">
+                        <button>Submit</button>
+                    </form>
+         </body>`);
+        res.write('</html>');
+
+        return res.end();
+    }
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', chunk => {
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const data = Buffer.concat(body).toString().split("=")[1];
+            fs.writeFileSync('message.text',data);
+        })
+        res.statusCode = 302; // redirection code
+        res.setHeader('Location', '/'); // redirection header
+        return res.end();
+
+    }
+    res.write("hello");
+    res.end();
+});
+
+server.listen(3000);
